e12f852433f39cfdc06a6f22e1d1da26a3ba3b81
kevin <kevinsua@gmail.com>
2023-01-02 19:30:21 +0100

    version 0.0.5

---
+++ src/JS/main.js
@@ -0,0 +1,6 @@
+const bmore = document.querySelector("#bmore");
+const links = document.querySelector("#links");
+
+bmore.addEventListener("click", (e) => {
+  links.classList.toggle("collapsed");
+});
